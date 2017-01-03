5053513ea04fdaaf1c8187c3d7dde90256cad9d8
ubuntu_ <metin@ubuntu.(none)>
2017-01-03 06:14:47 -0800

    Adding new directory

---
+++ xml/readexample.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <libxml/xmlmemory.h>
+#include <libxml/parser.h>
+
+int
+main(int argc, char **argv) {
+
+  char         *docname;
+  xmlDocPtr    doc;
+  xmlNodePtr   cur;
+  xmlChar      *uri;
+
+  if (argc <= 1) {
+    printf("Usage: %s docname\n", argv[0]);
+    return(0);
+  }
+
+  docname = argv[1];
+
+  doc = xmlParseFile(docname);
+  cur = xmlDocGetRootElement(doc);
+
+  cur = cur->xmlChildrenNode;
+  while (cur != NULL) {
+      if ((!xmlStrcmp(cur->name, (const xmlChar *)"reference"))) {
+        uri = xmlGetProp(cur, "uri");
+        printf("uri: %s\n", uri);
+        xmlFree(uri);
+      }
+      cur = cur->next;
+  }
+  xmlFreeDoc(doc);
+  return (1);
+}
